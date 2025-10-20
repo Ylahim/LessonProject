7b58c422e8e6ae4fdda6e2a07ae2af32a5f67275
Susmaher <troltom.122@gmail.com>
2025-10-20 10:56:19 +0200

    backend is van

---
+++ backend/Data/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        // Add database tables here
+        public DbSet<User> Users { get; set; }
+    }
+}
